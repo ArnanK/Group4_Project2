46f2f95fbebcf7b329724b6ceabb7d9ce6789428
ArnanK <arnank9@gmail.com>
2023-11-12 21:31:14 -0500

    stored procedures|Small Changes

--- Project2.LoadStarSchemaData/Project2.LoadStarSchemaData.sql
+++ Project2.LoadStarSchemaData/Project2.LoadStarSchemaData.sql
@@ -19,39 +19,41 @@ BEGIN
     --
     --	Drop All of the foreign keys prior to truncating tables in the star schema
  	--
-    EXEC  [Project2].[DropForeignKeysFromStarSchemaData] @GroupMemberUserAuthorizationKey = 1;
+    EXEC  [Project2].[DropForeignKeysFromStarSchemaData] @GroupMemberUserAuthorizationKey = 2;
 	--
 	--	Check row count before truncation
 	EXEC	[Project2].[ShowTableStatusRowCount]
-		@GroupMemberUserAuthorizationKey = -1,  -- Change -1 to the appropriate UserAuthorizationKey
+		@GroupMemberUserAuthorizationKey = 2,  -- Change -1 to the appropriate UserAuthorizationKey
 		@TableStatus = N'''Pre-truncate of tables'''
     --
     --	Always truncate the Star Schema Data
     --
-    EXEC  [Project2].[TruncateStarSchemaData] @GroupMemberUserAuthorizationKey = 1;
+    EXEC  [Project2].[TruncateStarSchemaData] @GroupMemberUserAuthorizationKey = 2;
     --
     --	Load the star schema
     --
-    EXEC  [Project2].[Load_DimProductCategory] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimProductSubcategory] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimProduct] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_SalesManagers] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimGender] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimMaritalStatus] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimOccupation] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimOrderDate] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimTerritory] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_DimCustomer] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
-    EXEC  [Project2].[Load_Data] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimProductCategory] @GroupMemberUserAuthorizationKey = 1;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimProductSubcategory] @GroupMemberUserAuthorizationKey = 1;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimProduct] @GroupMemberUserAuthorizationKey = 2;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_SalesManagers] @GroupMemberUserAuthorizationKey = 3;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimGender] @GroupMemberUserAuthorizationKey = 3;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimMaritalStatus] @GroupMemberUserAuthorizationKey = 4;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimOccupation] @GroupMemberUserAuthorizationKey = 4;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimOrderDate] @GroupMemberUserAuthorizationKey = 5;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimTerritory] @GroupMemberUserAuthorizationKey = 5;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_DimCustomer] @GroupMemberUserAuthorizationKey = 6;  -- Change -1 to the appropriate UserAuthorizationKey
+    EXEC  [Project2].[Load_Data] @GroupMemberUserAuthorizationKey = 6;  -- Change -1 to the appropriate UserAuthorizationKey
   --
     --	Recreate all of the foreign keys prior after loading the star schema
     --
  	--
 	--	Check row count before truncation
 	EXEC	[Project2].[ShowTableStatusRowCount]
-		@GroupMemberUserAuthorizationKey = -1,  -- Change -1 to the appropriate UserAuthorizationKey
+		@GroupMemberUserAuthorizationKey = 2,  -- Change -1 to the appropriate UserAuthorizationKey
 		@TableStatus = N'''Row Count after loading the star schema'''
 	--
-   EXEC [Project2].[AddForeignKeysToStarSchemaData] @GroupMemberUserAuthorizationKey = -1;  -- Change -1 to the appropriate UserAuthorizationKey
+   EXEC [Project2].[AddForeignKeysToStarSchemaData] @GroupMemberUserAuthorizationKey = 2;  -- Change -1 to the appropriate UserAuthorizationKey
     --
 END;
+
+
